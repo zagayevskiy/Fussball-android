dfacbfc7f09395a74049223c84337878b00461d8
Denis Zagayevskiy <zagayevskiy@gmail.com>
2014-07-10 20:44:24 +0600

    Crashlitycs added

--- src/com/zagayevskiy/fussball/SplashActivity.java
+++ src/com/zagayevskiy/fussball/SplashActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.crashlytics.android.Crashlytics;
 import com.zagayevskiy.fussball.api.ApiService;
 import com.zagayevskiy.fussball.api.Token;
 
@@ -12,6 +13,7 @@ public class SplashActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Crashlytics.start(this);
 		startService(new Intent(this, ApiService.class));
 		
 		if(Token.getInstance().hasToken(this)){
